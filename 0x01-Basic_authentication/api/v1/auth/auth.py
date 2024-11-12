fde26c1cce3f7e66688d4380c6e52418d46248d9
wandilemawelela <wandile@gmail.com>
2024-11-12 18:02:50 +0000

    [ADD] Completed task 3: a class to manage the API authentication.

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+# api/v1/auth/auth.py
+
+from typing import List, TypeVar
+from flask import request
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Public method to check if authentication is required.
+        Currently always returns False.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Public method to get the value of the Authorization header.
+        Currently always returns None.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Public method to get the current user.
+        Currently always returns None.
+        """
+        return None
