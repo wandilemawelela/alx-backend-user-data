cac8111288e2fd55fd6b1d3ebc6b8f7b387fb979
wandilemawelela <wandile@gmail.com>
2024-11-14 16:48:43 +0000

    [ADD] Completed task 5: Update the method def authorization_header(self, request=None) -> str: in api/v1/auth/auth.py:

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+
 """
 Route module for the API
 """
+
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
@@ -12,25 +14,52 @@ app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+# Initialize auth variable to None
+auth = None
+
+# Set auth based on AUTH_TYPE environment variable
+auth_type = getenv("AUTH_TYPE")
+if auth_type:
+    if auth_type == "auth":
+        from api.v1.auth.auth import Auth
+        auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """ Method to handle before each request """
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if request.path in excluded_paths:
+        return
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler
-    """
+    """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def unauthorized(error) -> str:
-    """ Forbidden handler
-    """
+def forbidden(error) -> str:
+    """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
 
 
